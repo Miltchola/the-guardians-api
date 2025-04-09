1e9d619139d3356ca8f7d3a3209b42688062799c
Milton Kiefer <145204997+Miltchola@users.noreply.github.com>
2025-04-09 18:01:31 -0300

    Add files via upload

--- src/components/Carrossel/Carrossel.tsx
+++ src/components/Carrossel/Carrossel.tsx
@@ -12,6 +12,7 @@ import img1 from '../../assets/images/Joshua e Henrique.jpg';
 import img2 from '../../assets/images/Jorge chegando.jpg';
 import img3 from '../../assets/images/Manuel e Poker Guy.jpg';
 import img4 from '../../assets/images/Joshua apresentação.png';
+import img5 from '../../assets/images/Nicolas, Henrique e Jade.jpg'
 
 
 // Imagens que vão passar no Carrossel
@@ -20,7 +21,8 @@ const data = [
     {id: '1', image: img1},
     {id: '2', image: img2},
     {id: '3', image: img3},
-    {id: '4', image: img4}
+    {id: '4', image: img4},
+    {id: '5', image: img5}
 ]
 
 const Carrossel = () => {
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,24 +3,31 @@ import './Footer.css'
 import insta from '../../assets/icons/Instagram logo.png'
 import webtoon from '../../assets/icons/Webtoon logo.png'
 
+import logo from '../../assets/icons/Teste Logo - Guardians (branca).png'
+import logoTexto from '../../assets/icons/Logo - Guardians texto.png'
+
+
 const Footer = () => {
     return (
         <div className="footer-section">
             <div className="social-media">
-                <a href="">
-                    <img className="logo" src={insta} alt="Instagram Logo" />@the.guardians.official
+                <a className="social-media-link" href="">
+                    <img className="logo" src={insta} alt="Instagram Logo" />
+                    <h4 className="media-text">@the.guardians.official</h4>
                 </a>
                 
-                <a href="">
-                    <img className="logo" src={webtoon} alt="Webtoon Logo" />The Guardians - Protectors of Mankind
+                <a className="social-media-link" href="">
+                    <img className="logo" src={webtoon} alt="Webtoon Logo" />
+                    <h4 className="media-text">The Guardians - Protectors of Mankind</h4>
                 </a>
             </div>
 
-            <hr />
-
-            <div>
-
+            <div className="footer-logo">
+                <img className="logo-icon" src={logo} alt="logo" />
+                <img className="logo-text" src={logoTexto} alt="logoTexto" />
             </div>
+
+            <hr className="hr-section" />
 
             <div className="footer-rights">
                 <p>&copy; 2025 The Guardians - Protectors of Mankind. All rights reserved.</p>
